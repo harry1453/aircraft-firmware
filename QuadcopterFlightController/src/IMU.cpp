b10cecd6da8007ec5edda7db52a38899e79f2dea
Harry Phillips <harryjamesphillips@gmail.com>
2020-08-16 12:09:56 +0100

    Tweaking IMU configuration to combat issues experienced
    Tweaks:
    * Actually bother doing the full calibration
    * Increase full scale as the drone can easily rotate at over 250 degrees per second and can easily accelerate at over 2g

--- QuadcopterFlightController/src/IMU.cpp
+++ QuadcopterFlightController/src/IMU.cpp
@@ -2,17 +2,10 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-#include <MPU6050_6Axis_MotionApps20.h>
+#include <MPU6050_6Axis_MotionApps_V6_12.h>
 
 namespace IMU {
     namespace { // private
-        Quaternion q;           // [w, x, y, z]         quaternion container
-        VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-        VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-        VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-        VectorFloat gravity;    // [x, y, z]            gravity vector
-        float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
         uint8_t fifoBuffer[64]; // FIFO storage buffer
         uint16_t packetSize1;
         uint16_t packetSize2;
@@ -37,31 +30,40 @@ namespace IMU {
             return;
         }
 
+        imu1.setFullScaleAccelRange(MPU6050_ACCEL_FS_8);
+        imu1.setFullScaleGyroRange(MPU6050_GYRO_FS_1000);
         auto devStatus = imu1.dmpInitialize();
-        imu1.setXGyroOffset(220);
-        imu1.setYGyroOffset(76);
-        imu1.setZGyroOffset(-85);
-        imu1.setZAccelOffset(1788);
+        imu1.setXAccelOffset(-897);
+        imu1.setYAccelOffset(1197);
+        imu1.setZAccelOffset(1420);
+        imu1.setXGyroOffset(-188);
+        imu1.setYGyroOffset(-38);
+        imu1.setZGyroOffset(-4);
         if (devStatus != 0) {
             Serial.println("Could not initialize IMU1 (Error code " + String(devStatus));
-            return;
+            return; // TODO proper error handler
         }
+
+        imu2.setFullScaleAccelRange(MPU6050_ACCEL_FS_8);
+        imu2.setFullScaleGyroRange(MPU6050_GYRO_FS_1000);
         devStatus = imu2.dmpInitialize();
-        imu2.setXGyroOffset(220);
-        imu2.setYGyroOffset(76);
-        imu2.setZGyroOffset(-85);
-        imu2.setZAccelOffset(1788);
+        imu2.setXAccelOffset(829);
+        imu2.setYAccelOffset(-813);
+        imu2.setZAccelOffset(1473);
+        imu2.setXGyroOffset(37);
+        imu2.setYGyroOffset(29);
+        imu2.setZGyroOffset(11);
         if (devStatus != 0) {
             Serial.println("Could not initialize IMU2 (Error code " + String(devStatus));
             return;
         }
 
-        imu1.CalibrateAccel(6);
-        imu1.CalibrateGyro(6);
+        imu1.CalibrateAccel(5);
+        imu1.CalibrateGyro(5);
         imu1.PrintActiveOffsets();
 
-        imu2.CalibrateAccel(6);
-        imu2.CalibrateGyro(6);
+        imu2.CalibrateAccel(5);
+        imu2.CalibrateGyro(5);
         imu2.PrintActiveOffsets();
 
         imu1.setDMPEnabled(true);
@@ -85,6 +87,9 @@ namespace IMU {
                 fifoCount -= packetSize1;
             }
 
+            Quaternion q;           // [w, x, y, z]         quaternion container
+            VectorFloat gravity;    // [x, y, z]            gravity vector
+            float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
             imu1.dmpGetQuaternion(&q, fifoBuffer);
             imu1.dmpGetGravity(&gravity, &q);
             imu1.dmpGetYawPitchRoll(ypr, &q, &gravity);
